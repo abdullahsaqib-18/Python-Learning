9b2cdddd581834b8250ccd475f56b91a2f334f32
Abdullah Saqib <abdullahsaqib1822007@gmail.com>
2026-08-06 18:13:37 +0500

    commit

--- main.py
+++ main.py
@@ -1,113 +1,74 @@
+# money=int(input("mama givs me money"))
 
-p=20
+# if money==10:
+#     print("i will buy choco bar")
 
-q=5
+# elif money==20:
+#     print("i will buy mango dolly")
 
-print(p+q)
+# elif money==30:
+#     print("i will buy mango")
 
-print(p-q)
+# elif money==40:
+#     print("i will buy banana")
 
-print(p*q)
+# else:
+#     print("i will buy a cone")
 
-print(p/q)
+# #examples
 
-print(p//q)
+# num1=int(input("enter first number"))
+# #question no 1
+# num2=int(input("enter second number"))
 
-print(4**3)
+# if num1>num2:
+#     print(f"{num1} is greater and {num2} is smaller")
 
-print(32%5)
-
-print(43+4/2)
-
-#assignment operator
-
-a=34
-
-#compound assignment operators
-
-a=20
-
-a=a+20
-
-a=a+40
-
-a=a+60
-
-print(a)
-
-#we can also write in this way              #compound assignment operators are
-                                            #a-=,a+=,aa//=,a*=,a**=,a//=
-                                        
-
-a=20
-
-a+=20
-
-a+=40
-
-a+=60
-
-print(a)
-
-#comparison operators
-#comoarison gives true and fals result only
-
-a=14
-
-b=14
-
-print(a==b)
-
-print(a!=b)
-
-c=45
-
-d=45.5
-
-print(c==d)
-
-print(c!=d)
-
-print(c<d)
-
-print(c>d)
-
-#we can also use numbers to compare
-
-print(c<=d)
-
-print(45<=45)
-
-print(ord("A"))
-print(ord("B"))
-
-print("A"<="B")
-
-print("ABC"<"ACB")
-
-#WE CANNOT COMPARE AN INT VALUE TO A STRING
-
-print(12>11 and 14<17 and 876>7)
-
-print(12!=12 or 43<=42 or 67>65)
-
-print(not 12==12)
-
-#ifelse
-a=25
-
-if a>12:
-
-    print("i will do task A")
-
-else:
-    print("i will do task B")
-
-money=int(input("mama, give me money"))
-
-if money==10:
-    print("i will buy a choco bar icecream")
-else:
-    print("i will buy a mango dolly icecream")   
+# elif num2>num1:
+#     print(f"{num1} is smaller and {num2} is greater")
     
+# else:
+#     print(f"{num1} is smaller and {num2} is greater")
+
+#example 2
+
+# gender=input("enter gender as(male or female)")
+# if gender=="male":
+#     print("good morning sir")
+
+# elif gender=="female":
+
+#     print("good morning maam")
+
+# else:
+#     print("Unidentified gender")
+#example3
+# num=int(input("enter a number"))
+
+# if num%2==0:
+#     print("number is even")
+
+# else:
+#     print("number is odd")
+
+# name=input("enter your name")
+# age=int(input("enter your age"))
+
+# if age>=18:
+#     print(f"{name} is eligible for casting vote")
+# else:
+#     print("you are not valid voter")
+
+# #example 4
+
+# year=int(input("enter an year"))
+
+# if year%100==0 and year%400==0:
+#     print("this is a leap year")
+
+# elif year%4==0:
+#     print("this is a leap year")
+
+# else:
+#     print("this is not a leap year")
 
